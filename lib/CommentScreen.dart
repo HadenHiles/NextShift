4d9c004b244088cac35685a072529f91be3fcf68
HadenHiles <haden@howtohockey.com>
2020-11-11 12:59:19 -0500

    Resolves #10. Allow user to edit/delete their own comments

--- lib/CommentScreen.dart
+++ lib/CommentScreen.dart
@@ -22,14 +22,20 @@ class _CommentScreenState extends State<CommentScreen> {
 
   bool didFetchComments = false;
   List<CommentItem> fetchedComments = [];
+  CommentItem editComment;
 
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _commentController = TextEditingController();
+  TextEditingController _commentController = TextEditingController();
 
   _CommentScreenState({this.requestId, this.requestOwner});
 
   @override
   Widget build(BuildContext context) {
+    // Set the edit comment text if the user is editing a comment
+    if (editComment != null) {
+      _commentController.text = editComment.comment.comment;
+    }
+
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -40,30 +46,71 @@ class _CommentScreenState extends State<CommentScreen> {
               ? Container()
               : Form(
                   key: _formKey,
-                  child: ListTile(
-                    title: TextFormField(
-                      controller: _commentController,
-                      decoration: InputDecoration(labelText: 'Write a comment...'),
-                      onFieldSubmitted: addComment,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Please write a comment";
-                        }
+                  child: editComment != null
+                      ? ListTile(
+                          title: TextFormField(
+                            controller: _commentController,
+                            decoration: InputDecoration(labelText: 'Update comment...'),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return "Please write a comment";
+                              }
 
-                        return null;
-                      },
-                    ),
-                    trailing: OutlineButton(
-                      onPressed: () {
-                        addComment(_commentController.text);
-                      },
-                      borderSide: BorderSide.none,
-                      child: Icon(
-                        Icons.send,
-                        color: Theme.of(context).accentColor,
-                      ),
-                    ),
-                  ),
+                              return null;
+                            },
+                          ),
+                          trailing: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              OutlineButton(
+                                onPressed: () {
+                                  updateComment(editComment, _commentController.text);
+                                },
+                                borderSide: BorderSide.none,
+                                child: Icon(
+                                  Icons.check,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                              ),
+                              OutlineButton(
+                                onPressed: () {
+                                  deleteComment(editComment.comment);
+                                },
+                                borderSide: BorderSide.none,
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : ListTile(
+                          title: TextFormField(
+                            controller: _commentController,
+                            decoration: InputDecoration(labelText: 'Write a comment...'),
+                            onFieldSubmitted: addComment,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return "Please write a comment";
+                              }
+
+                              return null;
+                            },
+                          ),
+                          trailing: OutlineButton(
+                            onPressed: () {
+                              addComment(_commentController.text);
+                            },
+                            borderSide: BorderSide.none,
+                            child: Icon(
+                              Icons.send,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                        ),
                 ),
         ],
       ),
@@ -81,6 +128,7 @@ class _CommentScreenState extends State<CommentScreen> {
             this.fetchedComments = snapshot.data.docs
                 .map((data) => CommentItem(
                       comment: Comment.fromSnapshot(data),
+                      editCb: triggerEditComment,
                     ))
                 .toList();
 
@@ -98,6 +146,7 @@ class _CommentScreenState extends State<CommentScreen> {
     List<CommentItem> comments = snapshot
         .map((data) => CommentItem(
               comment: Comment.fromSnapshot(data),
+              editCb: triggerEditComment,
             ))
         .toList();
 
@@ -115,19 +164,57 @@ class _CommentScreenState extends State<CommentScreen> {
 
       // add comment to the current listview for an optimistic update
       setState(() {
-        fetchedComments = List.from(fetchedComments)..add(CommentItem(comment: Comment(displayName: currentUser.displayName, comment: comment, timestamp: Timestamp.now(), avatarUrl: currentUser.photoURL, userId: currentUser.uid)));
+        fetchedComments = List.from(fetchedComments)
+          ..add(
+            CommentItem(
+              comment: Comment(displayName: currentUser.displayName, comment: comment, timestamp: Timestamp.now(), avatarUrl: currentUser.photoURL, userId: currentUser.uid),
+              editCb: triggerEditComment,
+            ),
+          );
+
+        didFetchComments = false;
       });
     }
+  }
+
+  triggerEditComment(CommentItem comment) {
+    setState(() {
+      editComment = comment;
+      didFetchComments = false;
+    });
+  }
+
+  updateComment(CommentItem commentItem, String newComment) {
+    if (_formKey.currentState.validate()) {
+      _commentController.clear();
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        transaction.update(commentItem.comment.reference, {'comment': newComment});
+      });
+
+      triggerEditComment(null);
+    }
+  }
+
+  deleteComment(Comment comment) {
+    _commentController.clear();
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction.delete(comment.reference);
+    });
+
+    triggerEditComment(null);
   }
 }
 
 class CommentItem extends StatelessWidget {
-  const CommentItem({Key key, this.comment}) : super(key: key);
+  const CommentItem({Key key, this.comment, this.editCb}) : super(key: key);
 
   final Comment comment;
+  final Function editCb;
 
   @override
   Widget build(BuildContext context) {
+    final bool isOwner = comment.userId == FirebaseAuth.instance.currentUser?.uid;
+
     return Column(
       children: <Widget>[
         ListTile(
@@ -135,11 +222,29 @@ class CommentItem extends StatelessWidget {
           leading: CircleAvatar(
             backgroundImage: NetworkImage(comment.avatarUrl),
           ),
-          trailing: Text(
-            timeago.format(
-              comment.timestamp.toDate(),
-              locale: 'en_short',
-            ),
+          trailing: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                timeago.format(
+                  comment.timestamp.toDate(),
+                  locale: 'en_short',
+                ),
+              ),
+              isOwner
+                  ? Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: InkWell(
+                        child: Icon(Icons.edit),
+                        onTap: () {
+                          editCb(this);
+                        },
+                      ),
+                    )
+                  : Container(),
+            ],
           ),
         ),
         Divider(),
