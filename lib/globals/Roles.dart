374444bf1bf58392748c2ce7f2cf4b930ef493dc
HadenHiles <haden@howtohockey.com>
2020-12-14 12:11:02 -0500

    Add hadenhiles@gmail.com as an admin

--- lib/globals/Roles.dart
+++ lib/globals/Roles.dart
@@ -1,6 +1,7 @@
 class Roles {
   static final List<String> admins = [
-    "Jm3Beh4TYVPTm9Xj5guA67dDAvV2",
-    "Wip97tDGgrSyYO7Fr51hKY2D2R72",
+    "Jm3Beh4TYVPTm9Xj5guA67dDAvV2", // haden@howtohockey.com
+    "Wip97tDGgrSyYO7Fr51hKY2D2R72", // admin@howtohockey.com
+    "EWGdx4laKPNZxkq8qcMm6zZAD7u1", // hadenhiles@gmail.com
   ];
 }
